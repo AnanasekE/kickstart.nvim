91232bf424fdae68c56497dc0d9fdf6bdf43519b
AnanasekE <janeksambor@gmail.com>
2024-04-02 22:05:21 +0200

    update

--- init.lua
+++ init.lua
@@ -570,7 +570,6 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         html = {},
-        htmx = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
