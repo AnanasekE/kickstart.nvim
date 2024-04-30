8139d66f25c6aa885a9c542c31e697b6b0e8170e
AnanasekE <janeksambor@gmail.com>
2024-04-30 15:10:22 +0200

    added autopairs

--- init.lua
+++ init.lua
@@ -248,6 +248,12 @@ require('lazy').setup({
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
 
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+  },
+
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
 
