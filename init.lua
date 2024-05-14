622c85dfc9037b22e36e7e3594c932d39d690da1
AnanasekE <janeksambor@gmail.com>
2024-05-14 15:08:58 +0200

    updated harpoon config

--- init.lua
+++ init.lua
@@ -920,7 +920,7 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set('n', '<leader>ha', function()
-  harpoon:list():append()
+  harpoon:list():add()
 end, { desc = 'Add to harpoon list' })
 -- vim.keymap.set('n', '<C-e>', function()
 --   harpoon.ui:toggle_quick_menu(harpoon:list())
